1c0794a4f6453f1d2e1a4675a516937d6c159cf1
Melissa Reynolds <mkreynolds@cableone.net>
2017-01-25 19:51:42 +0000

    lesson 7 fixed test for create action

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#create action" do
     it "should successfully create a new gram in our database" do
-      post :create, gram: { messaage: 'Hello!'}
+      post :create, gram: { message: 'Hello!'}
       expect(response).to redirect_to root_path
 
       gram = Gram.last
